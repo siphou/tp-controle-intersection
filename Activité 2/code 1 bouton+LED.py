33898ae475f788df8257eb340eed72c53fa2635c
siphou <siphou14@gmail.com>
2025-10-13 14:33:21 +0200

    Update code 1 bouton+LED.py

--- Activité 2/code 1 bouton+LED.py
+++ Activité 2/code 1 bouton+LED.py
@@ -1,34 +1,54 @@
 from machine import Pin
-from time import sleep
+from time import sleep, ticks_ms
+
+DELAI_BASE = 5  
 
 led_rouge = Pin(18, Pin.OUT)
-led_orange = Pin(19, Pin.OUT)
 led_verte = Pin(20, Pin.OUT)
+led_orange = Pin(19, Pin.OUT)
 bp = Pin(15, Pin.IN, Pin.PULL_UP)
 
-def Attendre(delai):
-    for _ in range(delai):
-        if bp.value() == 0:
-            print("Passage demandé")
-        sleep(1)
+def allumer_led(pin):
+    pin.value(1)
 
-try:
-    while True:
-        led_rouge.off()
-        led_orange.off()
-        led_verte.on()
-        Attendre(10)
+def eteindre_led(pin):
+    pin.value(0)
 
-        led_verte.off()
-        led_orange.on()
-        Attendre(2)
+def attendre_verte(delai):
+    debut = ticks_ms()
+    while (ticks_ms() - debut) < delai * 1000:
+        if bp.value() == 0:  # 
+            print("passage ")
+            while bp.value() == 0:
+                pass  
+            sleep(0.2)  
+            break
+        sleep(0.1)
 
-        led_orange.off()
-        led_rouge.on()
-        Attendre(8)
+def sequence_feu(delai):
+    allumer_led(led_verte)
+    attendre_verte(delai)
+    eteindre_led(led_verte)
 
-except KeyboardInterrupt:
-    led_rouge.off()
-    led_orange.off()
-    led_verte.off()
+    allumer_led(led_orange)
+    sleep(delai / 2)
+    eteindre_led(led_orange)
 
+    allumer_led(led_rouge)
+    sleep(delai / 2)
+    eteindre_led(led_rouge)
+
+def extinction_leds():
+    eteindre_led(led_rouge)
+    eteindre_led(led_verte)
+    eteindre_led(led_orange)
+
+def main():
+    try:
+        while True:
+            sequence_feu(DELAI_BASE)
+    except KeyboardInterrupt:
+        extinction_leds()
+        print("Programme interrompu proprement.")
+
+main()
