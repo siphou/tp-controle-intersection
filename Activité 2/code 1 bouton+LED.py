7a397840c3a7a28362d349b1ec5fcf0b1f1142aa
siphou <siphou14@gmail.com>
2025-10-13 14:15:11 +0200

    Update and rename code 1 bouton+LED to code 1 bouton+LED.py

---
+++ Activité 2/code 1 bouton+LED.py
@@ -0,0 +1,34 @@
+from machine import Pin
+from time import sleep
+
+led_rouge = Pin(0, Pin.OUT)
+led_orange = Pin(1, Pin.OUT)
+led_verte = Pin(2, Pin.OUT)
+bp = Pin(15, Pin.IN, Pin.PULL_UP)
+
+def Attendre(delai):
+    for _ in range(delai):
+        if bp.value() == 0:
+            print("Passage demandé")
+        sleep(1)
+
+try:
+    while True:
+        led_rouge.off()
+        led_orange.off()
+        led_verte.on()
+        Attendre(10)
+
+        led_verte.off()
+        led_orange.on()
+        Attendre(2)
+
+        led_orange.off()
+        led_rouge.on()
+        Attendre(8)
+
+except KeyboardInterrupt:
+    led_rouge.off()
+    led_orange.off()
+    led_verte.off()
+
